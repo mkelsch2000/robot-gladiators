0eecd1e19fd26afba37c7e181cdbc3cfe6030696
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2021-12-26 23:10:06 -0700

    added some basic js to get players robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playName
+console.log("Hello there " + playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("your robot's name is " + playerName);
+
+// this creatues a function named "fight"
+// this is a declaration!
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+// this one is an expression!
+// var fight2 = function() {
+//     window.alert("The second fight has begun!");
+// }
+
+// fight2();
